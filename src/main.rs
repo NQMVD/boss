ae1f60f6ca8422e3e9d8f4afe5d30e45c25edfef
ArchNoah <noah.buergler@gmail.com>
2024-05-18 16:58:24 +0200

    added go installed

--- src/main.rs
+++ src/main.rs
@@ -17,49 +17,101 @@ fn main() {
             .stdout(Stdio::null())
             .stderr(Stdio::null())
             .status()
-         {
-            Ok(status) => if status.success() { installed_managers.push(*manager) },
-            Err(_) => todo!()
+        {
+            Ok(status) => {
+                if status.success() {
+                    installed_managers.push(*manager)
+                }
+            }
+            Err(_) => todo!(),
         }
     }
 
-    println!("Installed package managers: {}", installed_managers.join(", "));
+    println!(
+        "Installed package managers: {}",
+        installed_managers.join(", ")
+    );
 
     let mut results = vec![];
     for manager in &installed_managers {
         match *manager {
             "apt" => {}
-            "yay" => {
-                match Command::new("yay").arg("-Ss").arg(package_name).output() {
+            "yay" => match Command::new("yay").arg("-Ss").arg(package_name).output() {
+                Ok(output) => {
+                    if !output.stdout.is_empty() {
+                        let stdout: Vec<u8> = output.stdout;
+                        let stdout_string = String::from_utf8(stdout).unwrap();
+                        let mut lines = stdout_string.split('\n');
+                        let count = lines.clone().count();
+                        let line = lines.nth(count - 3).unwrap();
+                        let mut chunks = line.split_whitespace();
+                        let fullname = chunks.next().unwrap();
+                        let mut fullnamesplit = fullname.split('/');
+                        let _repo = fullnamesplit.next().unwrap();
+                        let name = fullnamesplit.next().unwrap();
+                        let version = chunks.next().unwrap();
+
+                        if package_name == name {
+                            if line.contains("Installed") {
+                                results.push(format!(
+                                    "I -   yay: {} {} [installed]",
+                                    fullname, version
+                                ));
+                            } else {
+                                results.push(format!("A -   yay: {} {}", fullname, version));
+                            }
+                        } else {
+                            results.push(format!("X -   yay: {} != {}", package_name, fullname));
+                        }
+                    }
+                }
+                Err(_) => results.push(format!("Error running command for {}", manager)),
+            },
+            "go" => {
+                // only installed packages, go doesnt have a search command
+                // TODO: create one?
+
+                match Command::new("go")
+                    .arg("version")
+                    .arg("-m")
+                    .arg("/home/noah/go/bin")
+                    .output()
+                {
                     Ok(output) => {
                         if !output.stdout.is_empty() {
                             let stdout: Vec<u8> = output.stdout;
                             let stdout_string = String::from_utf8(stdout).unwrap();
-                            let mut lines = stdout_string.split('\n');
-                            let count = lines.clone().count();
-                            let line = lines.nth(count-3).unwrap();
-                            let mut chunks = line.split_whitespace();
-                            let fullname = chunks.next().unwrap();
-                            let mut fullnamesplit = fullname.split('/');
-                            let _repo = fullnamesplit.next().unwrap();
-                            let name = fullnamesplit.next().unwrap();
-                            let version = chunks.next().unwrap();
+                            let lines = stdout_string.split('\n');
 
-                            if package_name == name {
-                                if line.contains("Installed") {
-                                    results.push(format!("I -   yay: {} {} [installed]", fullname, version));
-                                } else {
-                                    results.push(format!("A -   yay: {} {}", fullname, version));
+                            let filtered_lines: Vec<&str> = lines
+                                .filter(|line| line.contains("path") && !line.is_empty())
+                                .collect();
+                            for line in &filtered_lines {
+                                let mut chunks = line.split_whitespace();
+                                chunks.next();
+                                let fullname = chunks.next().unwrap();
+                                let mut fullnamesplit = fullname.split('/');
+                                fullnamesplit.next();
+                                let name = fullnamesplit.clone().last().unwrap();
+                                let repo = fullnamesplit.collect::<Vec<_>>().join("/");
+
+                                if package_name == name {
+                                    results.push(format!(
+                                        "I -    go: {} ({}) [installed]",
+                                        name, repo
+                                    ));
                                 }
-                            } else {
-                                results.push(format!("X -   yay: {} != {}", package_name, fullname));
                             }
+                        } else {
+                            println!("stdout is empty!");
+                            println!("{:?}", output);
                         }
                     }
-                    Err(_) => results.push(format!("Error running command for {}", manager)),
+                    Err(_) => {
+                        results.push(format!("Error running 'install --list' for {}", manager))
+                    }
                 }
-                        }
-            "go" => {}
+            }
             "cargo" => {
                 let mut installed = false;
                 // first check if installed
@@ -70,25 +122,36 @@ fn main() {
                             let stdout_string = String::from_utf8(stdout).unwrap();
                             let lines = stdout_string.split('\n');
 
-                            let filtered_lines: Vec<&str> = lines.filter(|line| !line.starts_with(' ') && !line.is_empty()).collect();
+                            let filtered_lines: Vec<&str> = lines
+                                .filter(|line| !line.starts_with(' ') && !line.is_empty())
+                                .collect();
                             for line in &filtered_lines {
                                 let mut chunks = line.split_whitespace();
                                 let name = chunks.next().unwrap();
                                 let version = chunks.next().unwrap();
 
                                 if package_name == name {
-                                    results.push(format!("I - cargo: {} {} [installed]", name, version));
+                                    results.push(format!(
+                                        "I - cargo: {} {} [installed]",
+                                        name, version
+                                    ));
                                     installed = true;
                                 }
                             }
                         }
                     }
-                    Err(_) => results.push(format!("Error running 'install --list' for {}", manager)),
+                    Err(_) => {
+                        results.push(format!("Error running 'install --list' for {}", manager))
+                    }
                 }
 
                 // check if availlable if not installed
                 if !installed {
-                    match Command::new("cargo").arg("search").arg(package_name).output() {
+                    match Command::new("cargo")
+                        .arg("search")
+                        .arg(package_name)
+                        .output()
+                    {
                         Ok(output) => {
                             if !output.stdout.is_empty() {
                                 let stdout: Vec<u8> = output.stdout;
@@ -102,9 +165,15 @@ fn main() {
                                 let description = chunks.collect::<Vec<_>>().join(" ");
 
                                 if package_name == name {
-                                    results.push(format!("A - cargo: {} {} {}", name, version, description));
+                                    results.push(format!(
+                                        "A - cargo: {} {} {}",
+                                        name, version, description
+                                    ));
                                 } else {
-                                    results.push(format!("X - cargo: {} != {} {}", package_name, name, description));
+                                    results.push(format!(
+                                        "X - cargo: {} != {} {}",
+                                        package_name, name, description
+                                    ));
                                 }
                             }
                         }
@@ -112,9 +181,8 @@ fn main() {
                     }
                 }
             }
-            &_ => todo!()
+            &_ => todo!(),
         }
-        
     }
 
     println!("\nResults:");
---
+++ target/debug/deps/pceo-519651aa42482c18.d
@@ -0,0 +1,9 @@
+/home/noah/repos/pceo/target/debug/deps/libpceo-519651aa42482c18.rmeta: src/main.rs Cargo.toml
+
+/home/noah/repos/pceo/target/debug/deps/pceo-519651aa42482c18.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
